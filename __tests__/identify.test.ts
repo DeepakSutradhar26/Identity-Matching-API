0c1616d3aea95d38ea5b1fbb181e15d189cd5962
Deepak Sutradhar <140784188+DeepakSutradhar26@users.noreply.github.com>
2026-03-14 09:40:03 +0530

    jest api

--- __tests__/identify.test.ts
+++ __tests__/identify.test.ts
@@ -32,4 +32,54 @@ describe("POST /identify", () => {
         expect(data.phoneNumbers).toContain("3345");
         expect(data.secondaryContactIds).toBe([]);
     });
+
+    test("When either email is matching", async () => {
+        const primary = await prisma.contact.create({
+            data : {
+                email : "adam@gmail.com",
+                phoneNumber : "4554",
+                linkPrecedence : "primary",
+            }
+        });
+
+        const res = await request(app)
+        .post("/identify")
+        .send({
+            email : "adam@gmail.com",
+            phoneNumber : "5668"
+        });
+
+        const data = res.body.contact;
+
+        expect(res.status).toBe(200);
+        expect(data.primaryContactId).toBe(primary.id);
+        expect(data.emails.length).toBe(1);
+        expect(data.phoneNumbers.length).toBe(2);
+        expect(data.secondaryContactIds.length).toBe(1);
+    });
+
+    test("When either phone number is matching", async () => {
+        const primary = await prisma.contact.create({
+            data : {
+                email : "adam@gmail.com",
+                phoneNumber : "4554",
+                linkPrecedence : "primary",
+            }
+        });
+
+        const res = await request(app)
+        .post("/identify")
+        .send({
+            email : "gkfs@gmail.com",
+            phoneNumber : "4554"
+        });
+
+        const data = res.body.contact;
+
+        expect(res.status).toBe(200);
+        expect(data.primaryContactId).toBe(primary.id);
+        expect(data.emails.length).toBe(2);
+        expect(data.phoneNumbers.length).toBe(1);
+        expect(data.secondaryContactIds.length).toBe(1);
+    });
 });
