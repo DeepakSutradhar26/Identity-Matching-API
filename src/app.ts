a726bfcfab5acb1b4e27340dbe3daf949e9077ea
Deepak Sutradhar <140784188+DeepakSutradhar26@users.noreply.github.com>
2026-03-15 16:54:46 +0530

    cors origin

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import path from "path";
+import cors from "cors";
 import { identityMatching } from "./routes/identifyContacts";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
+app.use(cors({
+    origin : "https://identity-matching-ui.vercel.app"
+}));
 
 app.post('/identify', identityMatching);
 
