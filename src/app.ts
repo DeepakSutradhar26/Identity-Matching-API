fb10ef547623c72938275e22d18beee34f89e1fc
Deepak Sutradhar <140784188+DeepakSutradhar26@users.noreply.github.com>
2026-03-15 15:42:14 +0530

    serverless

--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,4 @@ app.use(express.json());
 
 app.post('/identify', identityMatching);
 
-if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../../frontend/dist");
-
-  app.use(express.static(frontendPath));
-
-  app.use((req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
-}
-
 export default app;
